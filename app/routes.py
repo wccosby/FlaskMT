8c05d7f0d8a409f690b2b55e419de4a0bca5b2bf
wccosby <cosby.william.c@gmail.com>
2018-12-21 00:00:21 +1100

    initial templating tutorials done

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {"username": "myname"}
-	return "Hello, World"
+
+	## fabricating a list of posts
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in ...'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The avengers movie was so cool'
+		}
+	]
+	# pass data to the jinja2 placeholders through the render_template function
+	return render_template('index.html', title='Home', user=user, posts=posts)
 
 ### manual form of including html (non-templated)
 # this version could be useful for APIs
